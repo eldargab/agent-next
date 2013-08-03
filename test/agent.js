e8be6aa99783198c057dcc9eb42cbaea7c47a70b
Eldar Gabdullin <eldargab@gmail.com>
2013-08-03 18:22:17 +0400

    do not forget res.body.abort() for unconsumed bodies

--- test/agent.js
+++ test/agent.js
@@ -10,6 +10,10 @@ app.get('/hello', function(req, res) {
   res.send('hello')
 })
 
+app.get('/to-hello', function(req, res) {
+  res.redirect('/hello')
+})
+
 app.post('/echo', function(req, res) {
   res.writeHead(200, req.headers)
   req.pipe(res)
@@ -52,86 +56,103 @@ describe('Basic agent', function() {
     })
   })
 
-  it('Should transfer req.headers', function(done) {
-    agent
-    .post(u + '/echo')
-    .set('X-Requested-With', 'agent-next')
-    .end(function(err, res) {
-      res.headers.should.have.property('x-requested-with').be.equal('agent-next')
-      done()
-    })
-  })
-
-  describe('When req.body is a string', function() {
-    it('Should transfer it', function(done) {
+  describe('request', function() {
+    it('Should transfer req.headers', function(done) {
       agent
       .post(u + '/echo')
-      .send('Привет')
+      .set('X-Requested-With', 'agent-next')
       .end(function(err, res) {
-        if (err) return done(err)
-        res.body.consume('utf8', function(err, text) {
-          if (err) return done(err)
-          text.should.equal('Привет')
-          done()
-        })
-      })
-    })
-
-    it('Should set Content-Length header', function(done) {
-      agent
-      .post(u + '/echo')
-      .send('Привет')
-      .end(function(err, res) {
-        if (err) return done(err)
-        res.headers.should.have.property('content-length').be.equal('12')
+        res.headers.should.have.property('x-requested-with').be.equal('agent-next')
         done()
       })
     })
-  })
 
-  describe('When req.body is a buffer', function() {
-    it('Should transfer it', function(done) {
-      agent
-      .post(u + '/echo')
-      .send(new Buffer([0, 1, 2]))
-      .end(function(err, res) {
-        if (err) return done(err)
-        res.body.consume(function(err, buf) {
+    describe('When req.body is a string', function() {
+      it('Should transfer it', function(done) {
+        agent
+        .post(u + '/echo')
+        .send('Привет')
+        .end(function(err, res) {
           if (err) return done(err)
-          buf.length.should.equal(3)
-          buf[0].should.equal(0)
-          buf[1].should.equal(1)
-          buf[2].should.equal(2)
+          res.body.consume('utf8', function(err, text) {
+            if (err) return done(err)
+            text.should.equal('Привет')
+            done()
+          })
+        })
+      })
+
+      it('Should set Content-Length header', function(done) {
+        agent
+        .post(u + '/echo')
+        .send('Привет')
+        .end(function(err, res) {
+          if (err) return done(err)
+          res.headers.should.have.property('content-length').be.equal('12')
+          res.body.abort()
           done()
         })
       })
     })
 
-    it('Should set Content-Length header', function(done) {
+    describe('When req.body is a buffer', function() {
+      it('Should transfer it', function(done) {
+        agent
+        .post(u + '/echo')
+        .send(new Buffer([0, 1, 2]))
+        .end(function(err, res) {
+          if (err) return done(err)
+          res.body.consume(function(err, buf) {
+            if (err) return done(err)
+            buf.length.should.equal(3)
+            buf[0].should.equal(0)
+            buf[1].should.equal(1)
+            buf[2].should.equal(2)
+            done()
+          })
+        })
+      })
+
+      it('Should set Content-Length header', function(done) {
+        agent
+        .post(u + '/echo')
+        .send(new Buffer([0, 1, 2]))
+        .end(function(err, res) {
+          if (err) return done(err)
+          res.headers.should.have.property('content-length').be.equal('3')
+          res.body.abort()
+          done()
+        })
+      })
+    })
+
+    describe('When req.body is a stream', function() {
+      it('Should transfer it', function(done) {
+        var stream = new Simple(fs.createReadStream(__filename))
+        agent
+        .post(u + '/echo')
+        .send(stream)
+        .end(function(err, res) {
+          if (err) return done(err)
+          res.body.consume('utf8', function(err, text) {
+            if (err) return done(err)
+            text.should.match(/Should transfer it/)
+            done()
+          })
+        })
+      })
+    })
+  })
+
+  describe('Redirects', function() {
+    it('Should follow redirects', function(done) {
       agent
-      .post(u + '/echo')
-      .send(new Buffer([0, 1, 2]))
+      .get(u + '/to-hello')
       .end(function(err, res) {
         if (err) return done(err)
-        res.headers.should.have.property('content-length').be.equal('3')
+        res.headers['x-hello'].should.equal('world')
+        res.body.abort()
         done()
-      })
-    })
-  })
-
-  describe('When req.body is a stream', function() {
-    it('Should transfer it', function(done) {
-      var stream = new Simple(fs.createReadStream(__filename))
-      agent
-      .post(u + '/echo')
-      .send(stream)
-      .end(function(err, res) {
-        if (err) return done(err)
-        res.body.consume('utf8', function(err, text) {
-          if (err) return done(err)
-          text.should.match(/Should transfer it/)
-          done()
-        })
       })
     })
   })
