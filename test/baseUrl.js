8c12808728e4392125eb247c97de4261e5971602
Eldar Gabdullin <eldargab@gmail.com>
2013-09-12 20:05:10 +0400

    fix tabs

--- test/baseUrl.js
+++ test/baseUrl.js
@@ -2,24 +2,24 @@ var should = require('should')
 var Agent = require('../index')
 
 describe('baseUrl(base) middleware', function() {
-	var cb = 'cb'
+  var cb = 'cb'
 
-	function test(given, expected) {
-		var req = new Agent.Request('GET', given)
-		var m = Agent.baseUrl('http://example.com/hello/hi')
-		var called = false
-		m(req, function(r, c) {
-			r.should.be.equal(req)
-			c.should.be.equal(cb)
-			r.url.toString().should.be.equal(expected)
-			called = true
-		}, cb)
-		called.should.be.true
-	}
+  function test(given, expected) {
+    var req = new Agent.Request('GET', given)
+    var m = Agent.baseUrl('http://example.com/hello/hi')
+    var called = false
+    m(req, function(r, c) {
+      r.should.be.equal(req)
+      c.should.be.equal(cb)
+      r.url.toString().should.be.equal(expected)
+      called = true
+    }, cb)
+    called.should.be.true
+  }
 
-	it('Should resolve request urls relative to given base', function() {
-		test('world', 'http://example.com/hello/world')
-		test('/index', 'http://example.com/index')
-		test('http://github.com', 'http://github.com/')
-	})
+  it('Should resolve request urls relative to given base', function() {
+    test('world', 'http://example.com/hello/world')
+    test('/index', 'http://example.com/index')
+    test('http://github.com', 'http://github.com/')
+  })
 })
